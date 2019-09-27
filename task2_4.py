62cf25ba85ea992e1fc67c134e7954c0980d2343
Ilia Kosolapov <ikosolapov@hotmail.com>
2019-09-27 14:27:55 +0300

    Homework 2 - Сделал, используя математическое выражение  @elpiankova

--- task2_4.py
+++ task2_4.py
@@ -1,7 +1,8 @@
 a = int(input("Введите значение:"))
-a = a+1
-if a%2 == 0:
-    print(a)
-else:
-    a = a+1
-    print(a)
+print("Следующее число: ", a//2*2+2)
+#a = a+1
+#if a%2 == 0:
+#    print(a)
+#else:
+#    a = a+1
+#    print(a)
